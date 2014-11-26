d673eb659715b283ad3390bd1666e6d4f8954fb7
Rachel Berry <crb@berrycs.com>
2014-11-26 05:27:12 +0000

    Add Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+   # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    if page_title.empty?
+      "Rachel Berry's Dream Companies"
+    else
+      "Why #{page_title} and Rachel Berry are a Perfect Match"
+    end
+  end
+  
 end
